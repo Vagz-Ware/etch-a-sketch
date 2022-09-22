19abf956ff77a0834000784ed00fb5fb79603559
Da_Loy <loyisondlovu6@gmail.com>
2022-09-22 17:23:09 +0200

    Added hover functions on my divs. when the mouse hovers over them then will chnage the background color of the divs.

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const container = document.getElementById("container");
 let rows = document.getElementsByClassName("gridRow");
 let cells = document.getElementsByClassName("cell");
 
-makeDefaultGrid();
+makeDefaultGrid()
 
 function makeDefaultGrid() {
     makeRows(16);
@@ -23,8 +23,18 @@ function makeColumns(cellNum) {
     for (i = 0; i < rows.length; i++) {
         for (j = 0; j < cellNum; j++) {
             let newCell = document.createElement("div");
+
+            newCell.onmouseover = function() {
+                this.style.backgroundColor = "blue";
+            }
+
+            newCell.onmouseout = function() {
+                this.style.backgroundColor = "red";
+            }
+            
             rows[j].appendChild(newCell).className = "cell";
         }
     }
     
 }
+
